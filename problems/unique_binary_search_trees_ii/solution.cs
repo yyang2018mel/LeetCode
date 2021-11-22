afa8cb67e9ef370cf6bca575875fe27f519caf9e
yyang2018mel <michael.yangyifan@gmail.com>
2021-11-22 21:03:32 +0000

    Sync LeetCode submission - Unique Binary Search Trees II (csharp)

--- problems/unique_binary_search_trees_ii/solution.cs
+++ problems/unique_binary_search_trees_ii/solution.cs
@@ -11,44 +11,81 @@
  *     }
  * }
  */
+
+using System.Collections.Immutable;
+
 public class Solution {
     
-    
-    
-    public IList<TreeNode> generateTrees(int n, int valIncrement = 0) {
-        
-        var trees = new List<TreeNode>();
-        
-        if (n == 0) {
-            trees.Add(null);
-            return trees;
+    static readonly ImmutableArray<TreeNode?> SizeZero = new[] { (TreeNode?)null }.ToImmutableArray();
+
+    private static ImmutableArray<TreeNode?> generateTrees(int size, int offset, ImmutableArray<TreeNode> leaves)
+    {
+        return size switch
+        {
+            0 => SizeZero,
+            1 => SizeOne(),
+            2 => SizeTwo(),
+            3 => SizeThree(),
+            _ => SizeN()
+        };
+
+        ImmutableArray<TreeNode?> SizeOne()
+        {
+            var builder = ImmutableArray.CreateBuilder<TreeNode?>(1);
+            builder.Add(leaves[offset + 1]);
+            return builder.MoveToImmutable();
         }
-        
-        if (n == 1) {
-            trees.Add(new TreeNode(1 + valIncrement, null, null));
-            return trees;
+
+        ImmutableArray<TreeNode?> SizeTwo()
+        {
+            var builder = ImmutableArray.CreateBuilder<TreeNode?>(2);
+            builder.Add(new TreeNode(1 + offset, null, leaves[2 + offset]));
+            builder.Add(new TreeNode(2 + offset, leaves[1 + offset], null));
+            return builder.MoveToImmutable();
         }
-    
-        for(var root = 1; root <= n; root++) {
-            
-            var lefts = generateTrees(root-1, valIncrement);
-            var rights = generateTrees(n-root, root+valIncrement);
-            
-            foreach (var left in lefts) {
-                foreach (var right in rights) {
-                    var tree = new TreeNode(root + valIncrement, left, right);
-                    trees.Add(tree);
+
+        ImmutableArray<TreeNode?> SizeThree()
+        {
+            var builder = ImmutableArray.CreateBuilder<TreeNode?>(5);
+            builder.Add(new TreeNode(1 + offset, null, new TreeNode(2 + offset, null, leaves[3 + offset])));
+            builder.Add(new TreeNode(1 + offset, null, new TreeNode(3 + offset, leaves[2 + offset], null)));
+            builder.Add(new TreeNode(2 + offset, leaves[1 + offset], leaves[3 + offset]));
+            builder.Add(new TreeNode(3 + offset, new TreeNode(1 + offset, null, leaves[2 + offset]), null));
+            builder.Add(new TreeNode(3 + offset, new TreeNode(2 + offset, leaves[1 + offset], null), null));
+            return builder.MoveToImmutable();
+        }
+
+        ImmutableArray<TreeNode?> SizeN()
+        {
+            var builder = ImmutableArray.CreateBuilder<TreeNode?>();
+
+            for (var lowerIdx = 1; lowerIdx <= size; lowerIdx++)
+            {
+                var lefts = generateTrees(lowerIdx - 1, offset, leaves);
+                var rights = generateTrees(size - lowerIdx, lowerIdx + offset, leaves);
+
+                foreach (var left in lefts)
+                {
+                    foreach (var right in rights)
+                    {
+                        var tree = new TreeNode(lowerIdx + offset, left, right);
+                        builder.Add(tree);
+                    }
                 }
             }
+            return builder.ToImmutable();
         }
-        
-        return trees;
     }
-    
-    
-    public IList<TreeNode> GenerateTrees(int n) {
-        
-        var trees = generateTrees(n);
+
+    public IList<TreeNode> GenerateTrees(int n)
+    {
+        var builder = ImmutableArray.CreateBuilder<TreeNode>(n + 1);
+        for (var i = 0; i <= n; i++)
+            builder.Add(new TreeNode(i, null, null));
+        var leaves = builder.MoveToImmutable();
+
+        var trees = generateTrees(n, 0, leaves);
+
         return trees;
     }
 }
