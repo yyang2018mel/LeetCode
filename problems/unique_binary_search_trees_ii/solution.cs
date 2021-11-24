96dae72193cf28087e32792622756a6965017e2a
yyang2018mel <michael.yangyifan@gmail.com>
2021-11-24 20:36:06 +0000

    Sync LeetCode submission - Unique Binary Search Trees II (csharp)

--- problems/unique_binary_search_trees_ii/solution.cs
+++ problems/unique_binary_search_trees_ii/solution.cs
@@ -16,53 +16,20 @@ using System.Collections.Immutable;
 
 public class Solution {
     
-    static readonly ImmutableArray<TreeNode?> SizeZero = new[] { (TreeNode?)null }.ToImmutableArray();
-
-    private static ImmutableArray<TreeNode?> generateTrees(int size, int offset, ImmutableArray<TreeNode> leaves)
+    private static ImmutableArray<TreeNode?> GenerateTrees(ImmutableArray<ImmutableArray<TreeNode?>[]> cached, int size, int offset)
     {
-        return size switch
-        {
-            0 => SizeZero,
-            1 => SizeOne(),
-            2 => SizeTwo(),
-            3 => SizeThree(),
-            _ => SizeN()
-        };
+        if (cached[size][offset].Length == 0)
+            cached[size][offset] = generate();
+        return cached[size][offset];
 
-        ImmutableArray<TreeNode?> SizeOne()
-        {
-            var builder = ImmutableArray.CreateBuilder<TreeNode?>(1);
-            builder.Add(leaves[offset + 1]);
-            return builder.MoveToImmutable();
-        }
-
-        ImmutableArray<TreeNode?> SizeTwo()
-        {
-            var builder = ImmutableArray.CreateBuilder<TreeNode?>(2);
-            builder.Add(new TreeNode(1 + offset, null, leaves[2 + offset]));
-            builder.Add(new TreeNode(2 + offset, leaves[1 + offset], null));
-            return builder.MoveToImmutable();
-        }
-
-        ImmutableArray<TreeNode?> SizeThree()
-        {
-            var builder = ImmutableArray.CreateBuilder<TreeNode?>(5);
-            builder.Add(new TreeNode(1 + offset, null, new TreeNode(2 + offset, null, leaves[3 + offset])));
-            builder.Add(new TreeNode(1 + offset, null, new TreeNode(3 + offset, leaves[2 + offset], null)));
-            builder.Add(new TreeNode(2 + offset, leaves[1 + offset], leaves[3 + offset]));
-            builder.Add(new TreeNode(3 + offset, new TreeNode(1 + offset, null, leaves[2 + offset]), null));
-            builder.Add(new TreeNode(3 + offset, new TreeNode(2 + offset, leaves[1 + offset], null), null));
-            return builder.MoveToImmutable();
-        }
-
-        ImmutableArray<TreeNode?> SizeN()
+        ImmutableArray<TreeNode?> generate()
         {
             var builder = ImmutableArray.CreateBuilder<TreeNode?>();
 
             for (var lowerIdx = 1; lowerIdx <= size; lowerIdx++)
             {
-                var lefts = generateTrees(lowerIdx - 1, offset, leaves);
-                var rights = generateTrees(size - lowerIdx, lowerIdx + offset, leaves);
+                var lefts  = GenerateTrees(cached, lowerIdx - 1, offset);
+                var rights = GenerateTrees(cached, size - lowerIdx, lowerIdx + offset);
 
                 foreach (var left in lefts)
                 {
@@ -73,19 +40,38 @@ public class Solution {
                     }
                 }
             }
+
             return builder.ToImmutable();
         }
     }
 
+    public static ImmutableArray<ImmutableArray<TreeNode?>[]> GenerateEmptyCache(int n)
+    {
+        var cacheBuilder = ImmutableArray.CreateBuilder<ImmutableArray<TreeNode?>[]>(n + 1);
+        for (var i = 0; i <= n; i++)
+        {
+            var empty = new ImmutableArray<TreeNode?>[n + 1];
+            for (var j = 0; j < n; j++)
+                empty[j] = ImmutableArray<TreeNode?>.Empty;
+            cacheBuilder.Add(empty);
+        }
+
+        var zeroSize = new[] { (TreeNode?)null }.ToImmutableArray();
+        for (var i=0; i <=n; ++i)
+        {
+            cacheBuilder[0][i] = zeroSize;
+        }
+
+        return cacheBuilder.MoveToImmutable();
+    }
+
     public IList<TreeNode> GenerateTrees(int n)
     {
-        var builder = ImmutableArray.CreateBuilder<TreeNode>(n + 1);
-        for (var i = 0; i <= n; i++)
-            builder.Add(new TreeNode(i, null, null));
-        var leaves = builder.MoveToImmutable();
+        var cache = GenerateEmptyCache(n);
 
-        var trees = generateTrees(n, 0, leaves);
+        var trees = GenerateTrees(cache, n, 0);
 
         return trees;
     }
+
 }
