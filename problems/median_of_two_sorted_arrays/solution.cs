2c039701d342fbc4ecb33db93b7e7ef7be61ddbd
yyang2018mel <michael.yangyifan@gmail.com>
2022-04-17 01:49:06 +0000

    Sync LeetCode submission - Median of Two Sorted Arrays (csharp)

--- problems/median_of_two_sorted_arrays/solution.cs
+++ problems/median_of_two_sorted_arrays/solution.cs
@@ -1,65 +1,92 @@
-public class Solution
+public class Solution 
 {
-    private int[] merge(int[] nums1, int[] nums2)
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2) 
     {
-        var merged = new int[nums1.Length + nums2.Length];
-        var idx = 0;
-        var idx1 = 0;
-        var idx2 = 0;
-
-        while(idx1 < nums1.Length && idx2 < nums2.Length)
+        var totalLen = nums1.Length + nums2.Length;
+        var halfLen = totalLen / 2;        
+     
+       if (nums1.Length == 0)
+        return 
+           totalLen % 2 != 0 
+           ? (double)nums2[halfLen] 
+           : (nums2[halfLen-1] + nums2[halfLen])/2.0;
+        
+        if (nums2.Length == 0)
+            return 
+            totalLen % 2 != 0 
+            ? (double)nums1[halfLen] 
+            : (nums1[halfLen-1] + nums1[halfLen])/2.0;
+     
+        var tillMedianLen = halfLen + 1;
+        var nums1MinItems = Math.Max(tillMedianLen - nums2.Length, 0);
+        var nums1MaxItems = Math.Min(tillMedianLen, nums1.Length);
+        
+        while (nums1MinItems <= nums1MaxItems)
         {
-            if (nums1[idx1] < nums2[idx2])
+            var nums1Items = (nums1MinItems + nums1MaxItems) / 2;
+            var nums2Items = tillMedianLen - nums1Items;
+            
+            var num1 =
+                nums1Items-1 >= 0 ? nums1[nums1Items-1] : Int32.MinValue;
+            
+            var num1_ = 
+                nums1Items < nums1.Length ? nums1[nums1Items] : Int32.MaxValue;
+            
+            var num2 = 
+                nums2Items-1 >= 0 ? nums2[nums2Items-1] : Int32.MinValue;
+            
+            var num2_ = 
+                nums2Items < nums2.Length ? nums2[nums2Items] : Int32.MaxValue;
+            
+            if (num1 >= num2)
             {
-                merged[idx++] = nums1[idx1];
-                idx1++;
+                if (num1 <= num2_)
+                {
+                    // find the median
+                    if (totalLen % 2 != 0)
+                    {
+                        return (double)num1;
+                    }
+                    else
+                    {
+                        // need to find the number that is immediately smaller than num1
+                        var _num1 = nums1Items-2 >= 0 ? nums1[nums1Items-2] : Int32.MinValue;
+                        var immediateSmaller = Math.Max(num2, _num1);
+                        return (immediateSmaller+num1)/2.0;
+                    }
+                    return totalLen % 2 != 0 ? (double)num1 : (num1+num2)/2.0;
+                }
+                else
+                {
+                    // need to reduce number of elements from nums1
+                    nums1MaxItems = nums1Items - 1;
+                }
             }
-            else if (nums1[idx1] > nums2[idx2])
+            else // (num1 < num2)
             {
-                merged[idx++] = nums2[idx2];
-                idx2++;
-            }
-            else
-            {
-                merged[idx++] = nums1[idx1];
-                merged[idx++] = nums2[idx2];
-                idx1++;
-                idx2++;
+                if (num2 <= num1_)
+                {
+                    // find the median
+                    if (totalLen % 2 != 0)
+                    {
+                        return (double)num2;
+                    }
+                    else
+                    {
+                        // need to find the number that is immediately smaller than num2
+                        var _num2 = nums2Items-2 >= 0 ? nums2[nums2Items-2] : Int32.MinValue;
+                        var immediateSmaller = Math.Max(num1,_num2);
+                        return (immediateSmaller+num2)/2.0;
+                    }
+                }
+                else
+                {
+                    // need to increase number of elements from nums1
+                    nums1MinItems = nums1Items + 1;
+                }
             }
         }
-
-        if(idx1 == nums1.Length)
-        {
-            while(idx2 < nums2.Length)
-            {
-                merged[idx++] = nums2[idx2++];
-            }
-        }
-        else if(idx2 == nums2.Length)
-        {
-            while(idx1 < nums1.Length)
-            {
-                merged[idx++] = nums1[idx1++];
-            }
-        }
-
-        return merged;
-    }
-
-    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
-    {
-        int mergedLen = nums1.Length + nums2.Length;
-        int[] merged = merge(nums1, nums2);
-
-        if (mergedLen % 2 != 0)
-        {
-            return merged[mergedLen / 2];
-        }
-        else
-        {
-            var idx1 = mergedLen / 2 - 1;
-            var idx2 = idx1 + 1;
-            return (merged[idx1] + merged[idx2]) / 2.0;
-        }            
+        
+        throw new Exception("Unexpected code path!");
     }
 }
